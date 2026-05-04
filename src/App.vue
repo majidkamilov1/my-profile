a73b7ed09041cd9eed916d416deb894209379a71
AbuKarabaev <karabaevabdulloh1808@icloud.com>
2026-05-05 02:43:27 +0600

    asd

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@ import Header from './components/header/index.vue'
 import Home from './components/home/index.vue'
 import About from './components/about/index.vue'
 import Projects from './components/projects/index.vue'
-import Skills from './components/skills/index.vue'
+import Footer from './components/footer/index.vue'
 </script>
 <template>
   <Header/>
   <Home/>
   <About/>
   <Projects/>
-  <Skills/>
+  <Footer/>
 </template>
 
